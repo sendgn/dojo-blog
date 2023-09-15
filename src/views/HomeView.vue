4e787bd74414934eb7df18581fc8b6bd14552f04
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-15 20:01:17 +0300

    Reusing composables, add tags

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,33 +5,19 @@
     <div v-if="posts.length">
       <PostList :posts="posts" />
     </div>
-    <div v-else style="color: crimson; font-size: 2em;">Loading...</div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
 import PostList from '../components/PostList.vue'
-import { ref } from 'vue'
+import getPosts from '../composables/getPosts'
 
 export default {
   name: 'HomeView',
   components: { PostList },
   setup() {
-    const posts = ref([])
-    const error = ref(null)
-
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if (!data.ok) {
-          throw new Error("Could't fetch the data")
-        }
-        posts.value = await data.json()
-      } catch (err) {
-        error.value = err.message
-      }
-    }
-
+    const { posts, error, load } = getPosts()
     load()
 
     return { posts, error }
