f58a167358301da192e8b220c4c8e0e0bd737ea1
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-16 12:08:36 +0300

    Style form

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -37,10 +37,68 @@ export default {
       }
       tag.value = ''
     }
-    
 
     return { title, body, tag, tags, handleKeydown }
   }
 }
 </script>
-<style></style>
+<style>
+  form {
+    max-width: 480px;
+    margin: 0 auto;
+    text-align: left;
+  }
+  input, textarea {
+    display: block;
+    margin: 10px 0;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10px;
+    border: 1px solid #eee;
+  }
+  input:focus, textarea:focus {
+    outline: 1px solid #ff8800;
+  }
+  textarea {
+    height: 160px;
+  }
+  label {
+    display: inline-block;
+    margin-top: 30px;
+    position: relative;
+    font-size: 20px;
+    color: white;
+    margin-bottom: 10px;
+  }
+  label::before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: #ff8800;
+    position: absolute;
+    z-index: -1;
+    padding-right: 40px;
+    left: -30px;
+    transform: rotateZ(-1.5deg);
+  }
+  button {
+    display: block;
+    margin-top: 30px;
+    background: #ff8800;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    font-size: 18px;
+    cursor: pointer;
+  }
+  .pill {
+    display: inline-block;
+    margin: 10px 10px 0 0;
+    color: #444;
+    background: #ddd;
+    padding: 8px;
+    border-radius: 20px;
+    font-size: 14px;
+  }
+</style>
